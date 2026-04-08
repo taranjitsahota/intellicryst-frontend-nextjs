e23a739f4cf988d00f607a055e9fc0c73849d358
taranjitsahota <taranjit.sahota@gmail.com>
2026-04-08 10:43:07 +0530

    added ga scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         {/* <!-- Google tag (gtag.js) --> */}
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-ZEEY001DKD"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DMV8P7QPT7"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -45,7 +45,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-ZEEY001DKD', {
+            gtag('config', 'G-DMV8P7QPT7', {
               page_path: window.location.pathname,
             });
           `}
